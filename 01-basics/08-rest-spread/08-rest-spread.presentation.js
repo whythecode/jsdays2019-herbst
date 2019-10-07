e697b6451c647a9341a1ed600456cc8803fa01f2
Sebastian Springer <info@sebastian-springer.com>
2019-10-07 14:49:31 +0200

    rest/spread fun

--- 01-basics/08-rest-spread/08-rest-spread.presentation.js
+++ 01-basics/08-rest-spread/08-rest-spread.presentation.js
@@ -1,9 +1,35 @@
 console.log('Rest/Spread');
 
-
 // Rest
 
+function myFunc(a, b, c, ...args) {
+  console.log(a, b, c);
+  return args;
+}
 
 // Spread
+const sebastian = [1, 2, 3];
+console.log(myFunc(...sebastian));
 
+// Destructuring
+const klaus = {
+  name: 'Klaus',
+  age: 42,
+};
 
+const { name: klausSeinFirstname } = klaus;
+
+console.log('ksfn', klausSeinFirstname);
+
+const arr = [klaus, klaus, klaus];
+
+const [, { name: k2SeinEigenenPersönlichenFirstname }] = arr;
+let k2SeinEigenenPersönlichenFirstname2 = arr[1].name;
+console.log(k2SeinEigenenPersönlichenFirstname);
+
+// Shortcut
+const name = 'someOtherName';
+const someOtherPerson = {
+  name,
+  age: 30,
+};
